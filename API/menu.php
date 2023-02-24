1da6dba73f91e8872a5196d5e8ff469f154f14d1
Rizqi Nur Andi Putra <92218072+Thecaracter@users.noreply.github.com>
2023-02-24 12:33:27 +0700

    Update menu.php

--- API/menu.php
+++ API/menu.php
@@ -1,30 +1,36 @@
 <?php
-
-//koneksi ke database
+// koneksi ke database
 include "../koneksi.php";
 
 // cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-$id = $_POST['id_kantin'];
-// Query untuk menampilkan data
-$sql = "SELECT * FROM menus WHERE id_kantin = $id;";
-$result = mysqli_query($conn, $sql);
 
-// membuat array kosong untuk menampung data
-$menus = array();
+// cek apakah parameter id_kantin ada dalam request
+if (isset($_POST['id_kantin'])) {
+    $id = $_POST['id_kantin'];
+    // Query untuk menampilkan data menu yang sesuai
+    $sql = "SELECT * FROM menus WHERE id_kantin = $id;";
+    $result = mysqli_query($conn, $sql);
 
-// melakukan loop untuk setiap baris hasil query
-while ($row = mysqli_fetch_assoc($result)) {
-    // menambahkan data ke dalam array
-    $menus[] = $row;
+    // membuat array kosong untuk menampung data
+    $menus = array();
+
+    // melakukan loop untuk setiap baris hasil query
+    while ($row = mysqli_fetch_assoc($result)) {
+        // menambahkan data ke dalam array
+        $menus[] = $row;
+    }
+
+    // menampilkan hasil dalam format JSON
+    echo json_encode($menus);
+} else {
+    // jika parameter id_kantin tidak ada dalam request, kirimkan response error
+    http_response_code(400);
+    echo json_encode(array('message' => 'Parameter id_kantin diperlukan'));
 }
-
-// menampilkan hasil dalam format JSON
-echo json_encode($menus);
 
 // menutup koneksi database
 mysqli_close($conn);
-
 ?>
